97c596d2e238c6ed06300960c52fdea73af58aa2
Michael Cohen <mcohenstuy@gmail.com>
2012-11-20 12:17:39 -0500

    Added scratch with some stuff

---
+++ SpaceViper/scratch.py
@@ -0,0 +1,10 @@
+#This is SpaceViper's sratchpad for brainstorming
+
+Ideas
+space shooter: bits where you shoot at alien ships
+terrain exploration: fly free around a planet, find items, fight ships, etc.
+-these sections would upgrade the ship [propulsion, heating [shoot time],
+                                        speed of shots, powerup power, etc.]
+
+Ship - The SpaceViper
+-crappy vessel (relatively speaking) initially
