bbb11d0e0728cebcdc178c418e14feea3e2d594b
Michael Cohen <mcohenstuy@gmail.com>
2012-11-27 12:36:10 -0500

    More scratch

--- SpaceViper/scratch.py
+++ SpaceViper/scratch.py
@@ -8,3 +8,22 @@ terrain exploration: fly free around a planet, find items, fight ships, etc.
 
 Ship - The SpaceViper
 -crappy vessel (relatively speaking) initially
+-power-ups are emblems
+-propulsion green rings thrown back the opposite direction of the ship; change
+when ship turned
+-Control:
+    In planet portions, like asteroids
+    In space shooters, maybe the same, maybe straightforward locked
+-Power-up ideas:
+    Centipede: Adds segments to your ship. These are extra hits.*
+    Big Fangs: Increases your fang firepower/size.
+    Rapid: Rapid fire.
+    Turbo: Increases speed when used. Triggered by user. Drops diff. numbers.*
+    Shield: Creates a temporary barrier that lasts 10 seconds.*
+
+    *These can be standard abilities when unlocked
+-Alternate Ships:
+    Cosmic Anaconda: Joke ship from the description. Like the standard ship,
+    but with alternate color pallate and propulsion.
+    BOSS SHIPS: These would be the bosses ships. These would be very difficult
+    to unlock, and lead to different story paths (you would face the SpaceViper)
