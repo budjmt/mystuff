f9d34792a105c4f7a588e7515204297cd8ae7643
Michael Cohen <mcohenstuy@gmail.com>
2012-12-03 10:36:31 -0500

    moar

--- SpaceViper/scratch.py
+++ SpaceViper/scratch.py
@@ -1,33 +1,37 @@
-#This is SpaceViper's sratchpad for brainstorming
+## This is SpaceViper's sratchpad for brainstorming
 
-Ideas
-space shooter: bits where you shoot at alien ships
-terrain exploration: fly free around a planet, find items, fight ships, etc.
--these sections would upgrade the ship [propulsion, heating [shoot time],
-                                        speed of shots, powerup power, etc.]
+## Ideas
+## space shooter: bits where you shoot at alien ships
+## terrain exploration: fly free around a planet, find items, fight ships, etc.
+## -these sections would upgrade the ship [propulsion, heating [shoot time],
+##                                         speed of shots, powerup power, etc.]
 
-Ship - The SpaceViper
--crappy vessel (relatively speaking) initially
--power-ups are emblems
--propulsion green rings thrown back the opposite direction of the ship; change
-when ship turned
--Control:
-    In planet portions, like asteroids
-    In space shooters, maybe the same, maybe straightforward locked
--Power-up ideas:
-    Centipede: Adds segments to your ship. These are extra hits.*
-    Big Fangs: Increases your fang firepower/size.
-    Rapid: Rapid fire.
-    Turbo: Increases speed when used. Triggered by user. Drops diff. numbers.*
-    Shield: Creates a temporary barrier that lasts 10 seconds.*
+## Ship - The SpaceViper
+## -crappy vessel (relatively speaking) initially
+## -power-ups are emblems
+## -propulsion green rings thrown back the opposite direction of the ship;
+##  change when ship turned
+## -Control:
+##     In planet portions, like asteroids
+##     In space shooters, maybe the same, maybe straightforward locked
+## -Power-up ideas:
+##    Centipede: Adds segments to your ship. These are extra hits.*
+##    Big Fangs: Increases your fang firepower/size.
+##    Rapid: Rapid fire.
+##    Turbo: Increases speed when used. Triggered by user. Drops diff. numbers.*
+##    Shield: Creates a temporary barrier that lasts 10 seconds.*
 
-    *These can be standard abilities when unlocked
--Alternate Ships:
-    CosmicAnaconda: Joke ship from the description. Like the standard ship,
-    but with alternate color pallate and propulsion.
-    BOSS SHIPS: These would be the bosses ships. These would be very difficult
-    to unlock, and lead to different story paths (you would face the SpaceViper)
-    BlackMamba: Black ship (obv) that is mostly cosmetic.
-    ThluffyBomber: Joke ship based on thluffy, the cs departments mascot.
-    Enemy Ships: Just standard enemy ships. This path would be uniform,
-    all enemies gone rogue.
+##     *These can be standard abilities when unlocked
+## -Alternate Ships:
+##  CosmicAnaconda: Joke ship from the description. Like the standard ship,
+##  but with alternate color pallate and propulsion.
+##  BOSS SHIPS: These would be the bosses ships. These would be very difficult
+##  to unlock, and lead to different story paths (you would face the SpaceViper)
+##  BlackMamba: Black ship (obv) that is mostly cosmetic.
+##  ThluffyBomber: Joke ship based on thluffy, the cs departments mascot.
+##  Enemy Ships: Just standard enemy ships. This path would be uniform,
+##  all enemies gone rogue.
+
+def moveEnemies(enemies):
+    for enemy in enemies:
+        enemy.move()
